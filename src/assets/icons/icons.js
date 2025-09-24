27c6598fa415cbb8d4d269d7d169588085b8fddc
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-24 18:22:39 +0900

    ✨ Feat: 이미지와 폰트 에셋 추가

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,21 @@
+import AddIcon from '@/assets/icons/ic-add.svg';
+import ArrowDownIcon from '@/assets/icons/ic-arrow-down.svg';
+import ArrowLeftIcon from '@/assets/icons/ic-arrow-left.svg';
+import ArrowRightIcon from '@/assets/icons/ic-arrow-right.svg';
+import CheckIcon from '@/assets/icons/ic-check.svg';
+import DeletedIcon from '@/assets/icons/ic-deleted.svg';
+import EditIcon from '@/assets/icons/ic-edit.svg';
+import PlusIcon from '@/assets/icons/ic-plus.svg';
+import ShareIcon from '@/assets/icons/ic-share.svg';
+
+export default {
+  AddIcon,
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  CheckIcon,
+  DeletedIcon,
+  EditIcon,
+  PlusIcon,
+  ShareIcon,
+};
---
+++ src/assets/images/images.js
@@ -0,0 +1,4 @@
+import mainImg1 from '@/assets/images/img-01.jpg';
+import mainImg2 from '@/assets/images/img-02.jpg';
+
+export default { mainImg1, mainImg2 };
